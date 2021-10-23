e2532abf062534d304f81d991ce236c960e69255
arsalanansari <arsalan.ansari@siden.io>
2021-10-22 21:29:24 -0400

    Added 2 more tests to validate that targets have no extra data

--- test/stream.test.js
+++ test/stream.test.js
@@ -32,10 +32,10 @@ describe("Stream Tests", function () {
     it("Verify All Inputs are accounted for in the Targets", function () {
       // Clone the initial set as we dont want to alter it, in case we need it for other test.
       const missingLinesSet = new Set(inputSet);
-      
+
       // Create sets for the differences between target and input files
-      let exceptSet1 = helper.except(missingLinesSet,eventSet1);
-      let exceptSet2 = helper.except(missingLinesSet,eventSet2);
+      let exceptSet1 = helper.except(missingLinesSet, eventSet1);
+      let exceptSet2 = helper.except(missingLinesSet, eventSet2);
       // Remove all entries from the input set that match, leaving behind only entries that
       // did not make it into either of the targets.
       helper.removeAll(missingLinesSet, eventSet2);
@@ -45,9 +45,21 @@ describe("Stream Tests", function () {
       logHelper.log(this, `event1 lines: ${eventSet1.size}`);
       logHelper.log(this, `event2 lines: ${eventSet2.size}`);
       logHelper.log(this, `input lines: ${inputSet.size}`);
-      logHelper.log(this, "Lines that were not processed correctly and EXACT matches were not found in the targets:\n" + logHelper.toArrayForLogs(missingLinesSet));
-      logHelper.log(this, "Target 1 incorrectly processed lines:\n" + logHelper.toArrayForLogs(exceptSet1));
-      logHelper.log(this, "Target 2 incorrectly processed lines:\n" + logHelper.toArrayForLogs(exceptSet2));
+      logHelper.log(
+        this,
+        "Lines that were not processed correctly and EXACT matches were not found in the targets:\n" +
+          logHelper.toArrayForLogs(missingLinesSet)
+      );
+      logHelper.log(
+        this,
+        "Target 1 incorrectly processed lines:\n" +
+          logHelper.toArrayForLogs(exceptSet1)
+      );
+      logHelper.log(
+        this,
+        "Target 2 incorrectly processed lines:\n" +
+          logHelper.toArrayForLogs(exceptSet2)
+      );
 
       // Assertion to validate that the missing set is empty.
       expect(
@@ -65,13 +77,52 @@ describe("Stream Tests", function () {
       // Logging for the report.
       logHelper.log(this, `intersections lines: ${interSet.size}`);
       logHelper.log(this, `event1 lines: ${eventSet1.size}`);
-      logHelper.log(this, 
-        "Lines that were found to exist in both target 1 AND target 2:\n" + logHelper.toArrayForLogs(interSet));
+      logHelper.log(
+        this,
+        "Lines that were found to exist in both target 1 AND target 2:\n" +
+          logHelper.toArrayForLogs(interSet)
+      );
 
       // Assertion to validate that the intersection set is empty.
       expect(
         interSet,
         `Data from input file seems to be on multiple target sources (See report logs for more information). Number of duplicates: ${interSet.size}`
+      ).to.be.empty;
+    });
+
+    it("Verify there is no extra data that exists in target 1 that does not exist in the input file", function () {
+      // Find any entries that exists only in target 1
+      let exceptSet1 = helper.except(inputSet, eventSet1);
+
+      // Logging for the report.
+      logHelper.log(
+        this,
+        "Data in target 1 that does not match any data from input file:\n" +
+          logHelper.toArrayForLogs(exceptSet1)
+      );
+
+      // Assertion to validate that the difference set is empty.
+      expect(
+        exceptSet1,
+        `Data from target 1 that does not match any data from input file. Number of extra lines of data: ${exceptSet1.size}`
+      ).to.be.empty;
+    });
+
+    it("Verify there is no extra data that exists in target 2 that does not exist in the input file", function () {
+      // Find any entries that exists only in target 2
+      let exceptSet2 = helper.except(inputSet, eventSet2);
+
+      // Logging for the report.
+      logHelper.log(
+        this,
+        "Data in target2 that does not match any data from input file:\n" +
+          logHelper.toArrayForLogs(exceptSet2)
+      );
+
+      // Assertion to validate that the difference set is empty.
+      expect(
+        exceptSet2,
+        `Data from target 2 that does not match any data from input file. Number of extra lines of data: ${exceptSet2.size}`
       ).to.be.empty;
     });
   });
